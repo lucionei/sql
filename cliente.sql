1c3dac27ca0a41af39426d63cf18497e1890433e
lucionei <lucionei@gmail.com>
2019-08-27 13:53:32 -0300

    Update cliente.sql
    Incluído comando  para exportar dados de tabelas do PostgresQL

--- cliente.sql
+++ cliente.sql
@@ -8,3 +8,9 @@ CREATE TABLE "CLIENTE" (
 );
 
 INSERT INTO "CLIENTE" (ID,NOME,CIDADE,EMAIL) VALUES (1,'Jack',7,'orci.lacus@velit.org'),(2,'Idona',3,'In.condimentum.Donec@tempus.com'),(3,'Myra',4,'ligula@sedfacilisis.co.uk'),(4,'Gretchen',10,'neque.Nullam@magnaNam.org'),(5,'Melyssa',10,'nibh.Aliquam@parturientmontesnascetur.co.uk'),(6,'Hu',8,'mattis@mifelisadipiscing.ca'),(7,'Debra',2,'semper.egestas.urna@non.net'),(8,'Leo',9,'ut.erat.Sed@vulputatevelit.ca'),(9,'Hilel',7,'Nulla.semper.tellus@et.net'),(10,'Linus',7,'erat.in@Nullasemper.net');
+
+COPY (
+    select *
+    from "CLIENTE"
+    order by id
+) TO 'C:\exemplo.txt' WITH NULL AS 'null';
