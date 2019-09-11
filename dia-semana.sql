efe26e6666dc56a77f5f3fe9a7d2b57810b3742b
Lucionei <lucionei@gmail.com>
2019-09-10 23:05:46 -0300

    Ajustes

--- dia-semana.sql
+++ dia-semana.sql
@@ -8,3 +8,16 @@ WHEN 4 THEN 'Quinta'
 WHEN 5 THEN 'Sexta'
 WHEN 6 THEN 'Sábado'
 END AS DiaSemana
+
+SELECT CASE EXTRACT( DOW FROM "PEDIDO".emissao)
+	   WHEN 0 THEN 'Domingo'
+	   WHEN 1 THEN 'Segunda'
+	   WHEN 2 THEN 'Terça'
+	   WHEN 3 THEN 'Quarta'
+	   WHEN 4 THEN 'Quinta'
+	   WHEN 5 THEN 'Sexta'
+	   WHEN 6 THEN 'Sábado'
+	   END AS dia_semana,
+count(*) as quantidade
+FROM "PEDIDO"
+group by 1
